ca8c68dbeec45badcc866f1d14c79c7fa8e6911d
Luis Carlos Elias <luis.elias@digitro.com>
2025-10-28 22:15:34 -0300

    refactor(api): simplifica api

--- src/api/v1/movies/handler.ts
+++ src/api/v1/movies/handler.ts
@@ -5,19 +5,11 @@ import { Movie } from '../../../types/index';
 const movies: Movie[] = [];
 
 export const getAllMovies = (req: Request, res: Response): void => {
-	try {
-		res.status(200).json({
-			success: true,
-			data: movies,
-			count: movies.length
-		});
-	} catch (error) {
-		res.status(500).json({
-			success: false,
-			message: 'Erro interno do servidor',
-			error: error instanceof Error ? error.message : 'Erro desconhecido'
-		});
-	}
+	res.status(200).json({
+		success: true,
+		data: movies,
+		count: movies.length
+	});
 };
 
 const getWinnersYear = (req: Request, res: Response): void => {

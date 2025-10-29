67ce1c345d90b1ae02236fb8fa14bad6bdaf32ea
Luis Carlos Elias <luis.elias@digitro.com>
2025-10-28 22:12:41 -0300

    chore(setup): cria método para finalizar servidor

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import app from './src/app.js';
 
 const PORT = process.env.PORT || 8901;
 
-if (process.env.NODE_ENV !== 'test') {
-	app.listen(PORT, () => {
-		console.log(`Servidor rodando na porta ${PORT}`);
-	});
-}
+const server = app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+export const closeServer = () => {
+  server.close();
+};
 
 export default app;

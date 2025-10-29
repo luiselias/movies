212268fe2814a5d8e25868b27214004f8210dd10
Luis Carlos Elias <luis.elias@digitro.com>
2025-10-28 22:01:45 -0300

    test: adiciona testes para novas apis DUMMY

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import app from '../index';
 
-describe('Aplicação', () => {
+describe('Aplicação de filmes', () => {
 	const resetScenario = () => {
 		jest.resetAllMocks();
 		jest.clearAllMocks();
@@ -18,6 +18,7 @@ describe('Aplicação', () => {
 	afterAll(() => {
 		resetScenario();
 	});
+
 	it('deve retornar a lista de filmes', async () => {
 		const response = await request(app).get('/api/v1/movies');
 
@@ -26,6 +27,53 @@ describe('Aplicação', () => {
 			count: 0,
 			data: [],
 			success: true
+		});
+	});
+
+	it('deve retornar a lista de filmes ganhadores por ano', async () => {
+		const response = await request(app).get('/api/v1/movies/winners');
+
+		expect(response.status).toBe(200);
+		expect(response.body).toEqual({
+			count: 0,
+			data: [],
+			success: true
+		});
+	});
+
+	it('deve retornar a lista de filmes ganhadores em intervalo de máximo e minímo', async () => {
+		const response = await request(app).get('/api/v1/movies/winners/range');
+
+		expect(response.status).toBe(200);
+		expect(response.body).toEqual({
+			"min": [
+				{
+					"producer": "Producer 1",
+					"interval": 1,
+					"previousWin": 2008,
+					"followingWin": 2009
+				},
+				{
+					"producer": "Producer 2",
+					"interval": 1,
+					"previousWin": 2018,
+					"followingWin": 2019
+				}
+			],
+			"max": [
+				{
+					"producer": "Producer 1",
+					"interval": 99,
+					"previousWin": 1900,
+					"followingWin": 1999
+				},
+				{
+					"producer": "Producer 2",
+					"interval": 99,
+					"previousWin": 2000,
+					"followingWin": 2099
+				}
+			]
 		});
 	});
 });

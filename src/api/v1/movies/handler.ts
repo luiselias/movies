f34948d214a9a346b2817619fba4c6f173a058fd
Luis Carlos Elias <luis.elias@digitro.com>
2025-10-28 22:02:15 -0300

    feat(api): cria APIs dummy de lista vencedores e intervalo

--- src/api/v1/movies/handler.ts
+++ src/api/v1/movies/handler.ts
@@ -5,19 +5,61 @@ import { Movie } from '../../../types/index';
 const movies: Movie[] = [];
 
 export const getAllMovies = (req: Request, res: Response): void => {
-    try {
-        res.status(200).json({
-            success: true,
-            data: movies,
-            count: movies.length
-        });
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: 'Erro interno do servidor',
-            error: error instanceof Error ? error.message : 'Erro desconhecido'
-        });
-    }
+	try {
+		res.status(200).json({
+			success: true,
+			data: movies,
+			count: movies.length
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: 'Erro interno do servidor',
+			error: error instanceof Error ? error.message : 'Erro desconhecido'
+		});
+	}
 };
 
-export default { getAllMovies };
+const getWinnersYear = (req: Request, res: Response): void => {
+	const winners = movies;
+	res.status(200).json({
+		success: true,
+		data: winners,
+		count: winners.length
+	});
+};
+
+const getWinnersYearRange = (req: Request, res: Response): void => {
+	res.status(200).json({
+		"min": [
+			{
+				"producer": "Producer 1",
+				"interval": 1,
+				"previousWin": 2008,
+				"followingWin": 2009
+			},
+			{
+				"producer": "Producer 2",
+				"interval": 1,
+				"previousWin": 2018,
+				"followingWin": 2019
+			}
+		],
+		"max": [
+			{
+				"producer": "Producer 1",
+				"interval": 99,
+				"previousWin": 1900,
+				"followingWin": 1999
+			},
+			{
+				"producer": "Producer 2",
+				"interval": 99,
+				"previousWin": 2000,
+				"followingWin": 2099
+			}
+		]
+	});
+};
+
+export default { getAllMovies, getWinnersYear, getWinnersYearRange };
--- src/api/v1/movies/route.ts
+++ src/api/v1/movies/route.ts
@@ -5,5 +5,7 @@ import handler from './handler.js';
 const router = Router();
 
 router.get('/api/v1/movies', handler.getAllMovies);
+router.get('/api/v1/movies/winners', handler.getWinnersYear);
+router.get('/api/v1/movies/winners/range', handler.getWinnersYearRange);
 
 export default router;
